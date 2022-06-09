d613a0472303a807060c000399505a47e04de6a4
BrianCosta2004 <brianhcosta@gmail.com>
2022-06-09 01:31:46 +0000

    Site final

--- Fatec/assets/js/main.js
+++ Fatec/assets/js/main.js
@@ -12,6 +12,15 @@
       return document.querySelector(el)
     }
   }
+  const input = document.getElementById("search-input");
+  const searchBtn = document.getElementById("search-btn");
+  
+  const expand = () => {
+    searchBtn.classList.toggle("close");
+    input.classList.toggle("square");
+  };
+  
+  searchBtn.addEventListener("click", expand);
 
   /**
    * Easy event listener function
